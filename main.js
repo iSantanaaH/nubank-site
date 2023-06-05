b64bde42dcfce31b3a2e8e4fdc0166ea1d5bcc4c
iSantanaaH <micaelsantana.dev@gmail.com>
2023-06-04 21:22:51 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const alertLinks = () => {
+    const message = `Atenção! Este site foi desenvolvido para fins de aprendizado pessoal e não está associado ao site oficial do Nubank. Por favor, lembre-se de não fornecer informações confidenciais neste site. Obrigado!`;
+    alert(message);
+};
